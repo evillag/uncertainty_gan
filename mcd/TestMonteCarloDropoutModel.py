12f760f2b6c12983d5442401e88b8c24cf46c5df
Efren <ejimenezdelgado@gmail.com>
2024-05-24 10:52:23 -0600

    new features

---
+++ mcd/TestMonteCarloDropoutModel.py
@@ -0,0 +1,26 @@
+import unittest
+
+class TestMonteCarloDropoutModel(unittest.TestCase):
+
+    def setUp(self):
+        self.model = MonteCarloDropoutModel('muon', 0.1, debug=False)
+
+    def test_model_initialization(self):
+        self.assertEqual(self.model.particle, 'muon')
+        self.assertEqual(self.model.dropout_rate, 0.1)
+        self.assertIsNotNone(self.model.get_generator())
+
+    def test_checkpoint_name(self):
+        self.assertEqual(self.model._get_checkpoint_name(), 'checkpoint_muon')
+
+    def test_model_string_representation(self):
+        self.assertEqual(str(self.model), 'muon_0.1')
+
+    def test_restore_model(self):
+        try:
+            self.model._restore_model()
+        except Exception as e:
+            self.fail(f"Model restore failed: {e}")
+
+if __name__ == '__main__':
+    unittest.main()
