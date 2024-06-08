c19b135cfc6ad0aac1702dfd9d69ff71437f2cbe
Esteban Villalobos <estvillalobos@estudiantec.cr>
2024-06-08 16:29:06 -0600

    Created evaluator classes.
    - Other fixes to tf_calculate_normalized_likelihoods (wip)

--- feature_densities/feature_density_evaluator.py
+++ feature_densities/feature_density_evaluator.py
@@ -14,7 +14,7 @@ EMBEDDING_LAYER = 14  # Index is 14 because layer indexing starts from 0
 def fit_kde(embeddings):
     kde_fit_functions = []
     for feature in range(embeddings.shape[1]):
-        kde_fit_functions.append(gaussian_kde(embeddings[:, feature]))
+        kde_fit_functions.append(gaussian_kde(embeddings[:, feature], bw_method=None))
     return kde_fit_functions
 
 
@@ -65,7 +65,7 @@ def tf_integration_likelihood(embeddings, kde_fit_functions, epsilon=0.01):
 
 
 # previously know as the range_mapping_likelihood.
-def tf_calculate_normalized_likelihoods(embeddings, kde_fit_functions):
+def tf_calculate_normalized_likelihoods(embeddings, kde_fit_functions, normalization_datapoints=100):
     """ This function calculates the likelihood of a given embedding point in the fitted KDE functions by estimating the
     value of the KDE function at the point divided by the maximum value of the KDE function for the KDE´s feature.
 
@@ -77,22 +77,22 @@ def tf_calculate_normalized_likelihoods(embeddings, kde_fit_functions):
     """
     likelihoods = []
     n_features = embeddings.shape[1]
-
-    x = np.linspace(embeddings.min(), embeddings.max(), 10000)
-    kde_max_values = [kde_fn(x).max() for kde_fn in kde_fit_functions]
-    tf_embeddings = tf.convert_to_tensor(embeddings)
+    x = tf.linspace(embeddings.min(), embeddings.max(), normalization_datapoints)
+    embeddings = tf.convert_to_tensor(embeddings)
 
     print('Calculating normalized likelihoods')
     for j in trange(n_features):
-        def normalize_kde_fn(x):
-            return kde_fit_functions[j](x) / kde_max_values[j]
+        def normalize_kde_fn(row_and_kde_max):
+            row, fit_max = row_and_kde_max
+            return kde_fit_functions[j](row) / fit_max
 
-        likelihoods.append(tf.map_fn(normalize_kde_fn, tf_embeddings[:, j]))
+        kde_max = tf.ones(embeddings.shape[0], dtype=tf.float32) * kde_fit_functions[j](x).max()
+        likelihoods.append(tf.map_fn(normalize_kde_fn, elems=(embeddings[:, j], kde_max)))
     return tf.transpose(tf.convert_to_tensor(likelihoods))
 
 
 # previously know as the range_mapping_likelihood.
-def calculate_normalized_likelihoods(embeddings, kde_fit_functions):
+def calculate_normalized_likelihoods(embeddings, kde_fit_functions, normalization_datapoints=100):
     """ Numpy version: This function calculates the likelihood of a given embedding point in the fitted KDE functions by
     estimating the value of the KDE function at the point divided by the maximum value of the KDE function for the KDE´s
     feature.
@@ -108,7 +108,7 @@ def calculate_normalized_likelihoods(embeddings, kde_fit_functions):
 
     likelihoods = np.zeros((n_samples, n_features))
 
-    x = np.linspace(embeddings.min(), embeddings.max(), 10000)
+    x = np.linspace(embeddings.min(), embeddings.max(), normalization_datapoints)
 
     for j in trange(n_features):
         kde_max = kde_fit_functions[j](x).max()
@@ -132,21 +132,24 @@ def create_embeddings_model(original_model: MonteCarloDropoutModel, embedding_la
     return embeddings_model
 
 
-def evaluate_model(model: MonteCarloDropoutModel, x_sample, known_embeddings=None, likelihood_method='integration'):
+def evaluate_model(model: MonteCarloDropoutModel, x_sample, kde_fit_functions, likelihood_method='integration',
+                   embeddings_model: Model = None, use_tf_version=False, normalization_datapoints=100):
     """ Arguments expected:
         - model: keras model: model to be evaluated
         - x_sample: np.array: sample to be evaluated
-        - known_embeddings: np.array: embeddings already known to the model, likely the embeddings from the training set
+        - kde_fit_functions: Any: Fitted KDE functions to be used in the evaluation, there is one per dimension of
+        x_sample
         - unc_method: str: method to be used to calculate the uncertainty. Options are 'integration' and 'normalized'
     Returns:
         - Tuple(tensorflow.Tensor, np.array): Tuple with uncertainty score (complement of likelihood) for the sample and
-        the generated targets
+         the generated targets.
     """
-    print('Generating an embeddings model')
-    embeddings_model = create_embeddings_model(model)
+    integration_fn = integration_likelihood if not use_tf_version else tf_integration_likelihood
+    normalized_fn = calculate_normalized_likelihoods if not use_tf_version else tf_calculate_normalized_likelihoods
 
-    print('Fitting KDE functions to known embeddings')
-    kde_fit_functions = fit_kde(known_embeddings)
+    if embeddings_model is None:
+        print('Generating an embeddings model')
+        embeddings_model = create_embeddings_model(model)
 
     print('Calculating sample´s embeddings')
     sample_embeddings, sample_predictions = embeddings_model.predict(x_sample)
@@ -154,9 +157,34 @@ def evaluate_model(model: MonteCarloDropoutModel, x_sample, known_embeddings=Non
     print('Estimating sample´s feature densities')
     feature_densities = tf.constant(np.ones((x_sample.shape[0], 1)))
     if likelihood_method == 'integration':
-        feature_densities = integration_likelihood(sample_embeddings, kde_fit_functions)
+        feature_densities = integration_fn(sample_embeddings, kde_fit_functions)
     elif likelihood_method == 'normalized':
-        feature_densities = calculate_normalized_likelihoods(sample_embeddings, kde_fit_functions)
+        feature_densities = normalized_fn(sample_embeddings, kde_fit_functions, normalization_datapoints)
 
     reduced_feature_densities = tf.math.reduce_mean(feature_densities, axis=1)
     return 1.0 - reduced_feature_densities, sample_predictions
+
+
+class FeatureDensityEvaluator:
+    def __init__(self, model: MonteCarloDropoutModel, known_embeddings=None, likelihood_method='integration',
+                 use_tf_version=False, normalization_datapoints=100):
+        self.model = model
+        self.known_embeddings = known_embeddings
+        self.likelihood_method = likelihood_method
+        self.use_tf_version = use_tf_version
+
+        print('Generating an embeddings model')
+        self.embeddings_model = create_embeddings_model(model)
+
+        print('Fitting KDE functions to known embeddings')
+        self.kde_fit_functions = fit_kde(known_embeddings)
+
+        # used in normalization method
+        self.normalization_datapoints = normalization_datapoints
+
+    def evaluate(self, x_sample):
+        uncertainties, generated_targets = evaluate_model(
+            self.model, x_sample, self.kde_fit_functions, self.likelihood_method, self.embeddings_model,
+            self.use_tf_version, self.normalization_datapoints)
+
+        return uncertainties, generated_targets
--- mcd/MCDEvaluator.py
+++ mcd/MCDEvaluator.py
@@ -28,6 +28,9 @@ class RichDLL(Enum):
 
 def evaluate_model(model, x_sample, ensemble_size=10):
     prediction_list = []
+
+    # An ensemble must be of at least two models in order to calculate
+    # the variance of the predictions
     if ensemble_size == 1:
         ensemble_size += 1
 
@@ -50,60 +53,46 @@ def evaluate_model(model, x_sample, ensemble_size=10):
 
 
 class MCDEvaluator:
-    def __init__(self, models, datasets, sub_sample_size, ensemble_size=10):
-        self.models = models
-        self.datasets = datasets
-        self.sub_sample_size = sub_sample_size
+    def __init__(self, model: MonteCarloDropoutModel, ensemble_size=10):
+        self.model = model
         self.ensemble_size = ensemble_size
 
-    def evaluate_all_models(self):
-        # Draw a sample of the datasets
-        x_sample, y_sample = subsample_dataset(self.datasets['feats_val'],
-                                               self.datasets['targets_val'],
-                                               self.sub_sample_size)
-        prediction_list = []
-        for name, model in self.models.items():
-            name_split = name.split('_')
-            particle = name_split[0]
-            dropout = name_split[1]
-            print(f"Evaluating Model for {particle} with dropout rate of {dropout}")
-            prediction_list.append(
-                evaluate_model(model, x_sample, self.ensemble_size)
-            )
-        return prediction_list
+    def evaluate(self, x_sample):
+        return evaluate_model(self.model, x_sample, self.ensemble_size)
 
 
 def test(debug=False):
-    if debug:
-        checkpoint_dp = '0.01'
-        checkpoint_base = '../test_bench/checkpoints/'
-        dropout_type = 'bernoulli_structured'
-        data_dir = '../test_bench/rich/'
-        particles = ['pion', 'muon']
-        dropouts = [0.05, 0.10]
-        sub_sample_size = .3
-
-        # If this cell is run more than once, previous test_bench are garbage collected and a Checkpoint warning is
-        # displayed, disregard it.
-        models = dict()
-        datasets = {particle: load_particle_datasets(particle, data_dir) for particle in particles}
-
-        for particle in particles:
-            for dp in dropouts:
-                # Test model creation with debug mode on
-                models[f"{particle}_{dp}"] = MonteCarloDropoutModel(
-                    particle,
-                    dropout_rate=dp,
-                    checkpoint_dir=checkpoint_base + get_checkpoint_name(particle, checkpoint_dp, dropout_type),
-                    debug=True
-                )
-            mcd_evaluator = MCDEvaluator(models, datasets[particle], sub_sample_size)
-            prediction_list = mcd_evaluator.evaluate_all_models()
-
-            for i, pred in enumerate(prediction_list):
-                print('{i} - Predictions Variance and Values:')
-                print(f'Variance:\n{pred[0]}')
-                print(f'Value:\n{pred[1]}')
+    pass
+    # if debug:
+    #     checkpoint_dp = '0.01'
+    #     checkpoint_base = '../test_bench/checkpoints/'
+    #     dropout_type = 'bernoulli_structured'
+    #     data_dir = '../test_bench/rich/'
+    #     particles = ['pion', 'muon']
+    #     dropouts = [0.05, 0.10]
+    #     sub_sample_size = .3
+    #
+    #     # If this cell is run more than once, previous test_bench are garbage collected and a Checkpoint warning is
+    #     # displayed, disregard it.
+    #     models = dict()
+    #     datasets = {particle: load_particle_datasets(particle, data_dir) for particle in particles}
+    #
+    #     for particle in particles:
+    #         for dp in dropouts:
+    #             # Test model creation with debug mode on
+    #             models[f"{particle}_{dp}"] = MonteCarloDropoutModel(
+    #                 particle,
+    #                 dropout_rate=dp,
+    #                 checkpoint_dir=checkpoint_base + get_checkpoint_name(particle, checkpoint_dp, dropout_type),
+    #                 debug=True
+    #             )
+    #         mcd_evaluator = MCDEvaluator(model, )
+    #         prediction_list = mcd_evaluator.evaluate_all_models()
+    #
+    #         for i, pred in enumerate(prediction_list):
+    #             print('{i} - Predictions Variance and Values:')
+    #             print(f'Variance:\n{pred[0]}')
+    #             print(f'Value:\n{pred[1]}')
 
 
 # Run tests
